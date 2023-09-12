adc0c6de4eabf91e639a28c5014e1f2f3a33e02e
spo1lr <choisb7366@gmail.com>
2023-09-12 23:43:40 +0900

    Feat: fine-tuning-job, list, delete

--- provider/openai.js
+++ provider/openai.js
@@ -1,17 +1,38 @@
 const OpenAI = require('openai');
 const fs = require("fs");
-const path = require("path");
 const axios = require("axios");
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function chat(modelId = '') {
-    const model = modelId ? `gpt-3.5-turbo:${modelId}` : 'gpt-3.5-turbo';
+async function chat(modelName = '') {
+    const model = modelName || 'gpt-3.5-turbo';
     const messages = [{role: 'user', content: 'hello'}];
 
     return openai.chat.completions.create({model, messages});
+}
+
+async function createCompletion(model = '', prompt = '') {
+    try {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        };
+
+        const data = {
+            model,
+            messages: [
+                {
+                    "role": "user",
+                    "content": `${prompt}`
+                }
+            ]
+        };
+        return await axios.post('https://api.openai.com/v1/chat/completions', data, {headers});
+    } catch (error) {
+        console.error('Error:', error.response.data);
+    }
 }
 
 async function fileUpload() {
@@ -38,4 +59,19 @@ async function createFineTuningJob(trainingFile = '') {
     }
 }
 
-module.exports = {chat, fileUpload, createFineTuningJob}
+async function deleteFineTuningJob(model = '') {
+    return openai.fineTunes.cancel(model);
+}
+
+async function fineTuningJobList() {
+    return openai.fineTunes.list();
+}
+
+module.exports = {
+    chat,
+    fileUpload,
+    createFineTuningJob,
+    FineTuningJobList: fineTuningJobList,
+    deleteFineTuningJob,
+    createCompletion
+}
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,55 @@ const express = require('express');
 const router = express.Router();
 const openai = require("../provider/openai");
 
-router.post('/', async (req, res) => {
-    // const chat = await openai.chat();
-    // const message = chat.choices[0].message;
+router.post('/chat/completion', async (req, res) => {
+    const model = req.body.model;
+    const prompt = req.body.prompt;
+
+    const completionChat = await openai.createCompletion(model, prompt);
+    console.log(completionChat.data.choices);
+    console.log(completionChat.choices);
+
+    // const message = completionChat.choices[0].message;
+
+    return res.send('ok');
+});
+
+router.post('/chat', async (req, res) => {
+    const chat = await openai.chat();
+    const message = chat.choices[0].message;
+
+    return res.json(message);
+});
+
+router.post('/fine-tuning', async (req, res) => {
     const fileUpload = await openai.fileUpload();
-    let fineTune = await openai.createFineTuningJob(fileUpload.id);
+    return res.json(fileUpload);
+});
+
+router.post('/fine-tuning/jobs', async (req, res) => {
+
+    const id = req.body.id;
+    if (!id) return res.status(400).json({result: 'fail', message: 'Please enter your id.'});
+
+    const fineTune = await openai.createFineTuningJob(id);
+    console.log(fineTune)
     return res.json(fineTune);
 });
+
+router.post('/fine-tuning/jobs/delete', async (req, res) => {
+    const model = req.body.id;
+    if (!model) return res.status(400).json({result: 'fail', message: 'Please enter your model name.'});
+
+    const fineTune = await openai.deleteFineTuningJob(model);
+    console.log(fineTune)
+    return res.json(fineTune);
+});
+
+router.get('/fine-tuning/jobs', async (req, res) => {
+    const fineTune = await openai.FineTuningJobList();
+    console.log(fineTune)
+    return res.json(fineTune);
+});
+
 
 module.exports = router;
